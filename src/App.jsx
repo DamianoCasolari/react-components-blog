b6f7e8f10f36d91f6416ea7767e3c1c444aba973
DamianoCasolari <damianocasolari@gmail.com>
2023-11-29 19:39:32 +0100

    experiment tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./css/index.css";
+
+// Import components 
+import { AppHeader } from './components/AppHeader'
+import { AppFooter } from './components/AppFooter'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='WebPage'>
+        <AppHeader />
+        <main className='vh100-header-footer flex items-center justify-center'>
+          <div className="card h-72 w-9/12 md:w-6/12 bg-black rounded-2xl">
+
+          </div>
+        </main>
+        <AppFooter />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
